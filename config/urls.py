58249234cf3ab570ad4c6e13603c11de04ed0500
SEYEON OH <seyeondoh@gmail.com>
2024-04-16 21:59:17 +0900

    4.16 upload

--- config/urls.py
+++ config/urls.py
@@ -24,4 +24,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('magic/', include('magic.urls')),
     path('', index, name='index'),
+    path('customer/', include('customer.urls')),
+
 ]+ static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
